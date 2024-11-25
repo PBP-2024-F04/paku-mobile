68395508a50bb101d2622030b7e3c37b37833c8e
secona <77039267+secona@users.noreply.github.com>
2024-11-25 16:21:15 +0700

    feat: landing page

---
+++ lib/screens/landing.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class LandingPage extends StatelessWidget {
+  const LandingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text("PaKu"),
+      ),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Welcome to PaKu!'),
+          ],
+        ),
+      ),
+    );
+  }
+}
