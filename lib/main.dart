0fb80c5d07ca17e16b429e927cb7a4579343e209
secona <77039267+secona@users.noreply.github.com>
2024-12-02 15:57:08 +0700

    feat: add login form

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:paku/screens/landing.dart';
+import 'package:paku/screens/accounts/login.dart';
+// import 'package:paku/screens/landing.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const LandingPage(),
+      home: const LoginPage(),
     );
   }
 }
--- lib/screens/accounts/login.dart
+++ lib/screens/accounts/login.dart
@@ -8,10 +8,75 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  String _username = "";
+  String _password = "";
+  
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("Login Page")),
+    return Scaffold(
+      body: Form(
+        key: _formKey,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: "Name",
+                  labelText: "Name",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                ),
+                onChanged: (String? value) => setState(() => _username = value!),
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: "Password",
+                  labelText: "Password",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                ),
+                obscureText: true,
+                onChanged: (String? value) => setState(() => _password = value!),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        title: const Text('Login'),
+                        content: SingleChildScrollView(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text('Username: $_username'),
+                              Text('Password: $_password'),
+                            ],
+                          ),
+                        ),
+                        actions: [
+                          TextButton(
+                            child: const Text('OK'),
+                            onPressed: () {
+                              Navigator.pop(context);
+                              _formKey.currentState?.reset();
+                            },
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                },
+                child: const Text('Login'),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
