a833a5444a0f6308997bb09bc8aabdb22942d4f6
secona <77039267+secona@users.noreply.github.com>
2024-12-04 22:37:13 +0700

    feat: simple search bar

--- lib/screens/timeline/timeline.dart
+++ lib/screens/timeline/timeline.dart
@@ -1,16 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:paku/colors.dart';
 
-class TimelinePage extends StatelessWidget {
+class TimelinePage extends StatefulWidget {
   const TimelinePage({super.key});
 
+  @override
+  State<TimelinePage> createState() => _TimelinePageState();
+}
+
+class _TimelinePageState extends State<TimelinePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Timeline"),
       ),
-      body: const Text("Timeline"),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(30),
+        child: Column(
+          children: [
+            SearchBar(
+              hintText: 'Search...',
+              elevation: const WidgetStatePropertyAll(0),
+              shape: const WidgetStatePropertyAll(
+                RoundedRectangleBorder(
+                  borderRadius: BorderRadius.zero,
+                  side: BorderSide(
+                    color: TailwindColors.mossGreenDark,
+                    width: 2,
+                  ),
+                ),
+              ),
+              trailing: [
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.search),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: TailwindColors.sageDark,
         foregroundColor: Colors.white,
