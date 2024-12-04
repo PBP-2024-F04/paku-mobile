c554a58b92b1582d1d45dc30750937e69204823d
secona <77039267+secona@users.noreply.github.com>
2024-12-04 16:47:13 +0700

    feat: register page styles

--- lib/screens/accounts/register.dart
+++ lib/screens/accounts/register.dart
@@ -17,19 +17,51 @@ class _RegisterPageState extends State<RegisterPage> {
         child: SingleChildScrollView(
           child: Column(
             children: [
+              Align(
+                alignment: Alignment.center,
+                child: Text(
+                  "Create Account",
+                  style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                    fontSize: 40,
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment.center,
+                child: Text(
+                  "Buat akun PaKu dan mulai jelajahi Kuliner Palu!",
+                  style: Theme.of(context).textTheme.labelLarge,
+                ),
+              ),
+              const SizedBox(height: 25),
               ElevatedButton(
-                child: const Text("Register as Foodie"),
+                style: ElevatedButton.styleFrom(
+                  elevation: 0,
+                  backgroundColor: Theme.of(context).colorScheme.primary,
+                  foregroundColor: Theme.of(context).colorScheme.surface,
+                  shape: const RoundedRectangleBorder(borderRadius: BorderRadius.zero),
+                  fixedSize: const Size(200, 30),
+                ),
                 onPressed: () => Navigator.push(
                   context,
                   MaterialPageRoute(builder: (_) => const RegisterFoodiePage()),
                 ),
+                child: const Text("Buat akun Foodie"),
               ),
+              const SizedBox(height: 5),
               ElevatedButton(
-                child: const Text("Register as Merchant"),
+                style: ElevatedButton.styleFrom(
+                  elevation: 0,
+                  backgroundColor: Theme.of(context).colorScheme.primary,
+                  foregroundColor: Theme.of(context).colorScheme.surface,
+                  shape: const RoundedRectangleBorder(borderRadius: BorderRadius.zero),
+                  fixedSize: const Size(200, 30),
+                ),
                 onPressed: () => Navigator.push(
                   context,
                   MaterialPageRoute(builder: (_) => const RegisterMerchantPage()),
                 ),
+                child: const Text("Buat akun Merchant"),
               ),
             ],
           ),
