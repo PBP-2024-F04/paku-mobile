5d406f473c7c3708819907887b57147229f61c00
secona <77039267+secona@users.noreply.github.com>
2024-12-04 15:25:30 +0700

    feat: implement registration forms

--- lib/screens/accounts/register_foodie.dart
+++ lib/screens/accounts/register_foodie.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+// username, password1, password2, full_name
+
 class RegisterFoodiePage extends StatefulWidget {
   const RegisterFoodiePage({super.key});
 
@@ -8,10 +10,108 @@ class RegisterFoodiePage extends StatefulWidget {
 }
 
 class _RegisterFoodiePageState extends State<RegisterFoodiePage> {
+  final _formKey = GlobalKey<FormState>();
+
+  String _username = "";
+  String _password1 = "";
+  String _password2 = "";
+  String _fullName = "";
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("Register Foodie Page")),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Register Foodie"),
+      ),
+      body: Form(
+        key: _formKey,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: "Username",
+                  labelText: "Username",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                ),
+                onChanged: (String? value) => setState(() => _username = value!),
+              ),
+
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: "Password",
+                  labelText: "Password",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                ),
+                obscureText: true,
+                onChanged: (String? value) => setState(() => _password1 = value!),
+              ),
+
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: "Password Confirmation",
+                  labelText: "Password Confirmation",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                ),
+                obscureText: true,
+                onChanged: (String? value) => setState(() => _password2 = value!),
+              ),
+
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: "Full Name",
+                  labelText: "Full Name",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                ),
+                onChanged: (String? value) => setState(() => _fullName = value!),
+              ),
+
+              ElevatedButton(
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        title: const Text('Register Foodie'),
+                        content: SingleChildScrollView(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text('Username: $_username'),
+                              Text('Password: $_password1'),
+                              Text('Password Confirmation: $_password2'),
+                              Text('Full Name: $_fullName'),
+                            ],
+                          ),
+                        ),
+                        actions: [
+                          TextButton(
+                            child: const Text('OK'),
+                            onPressed: () {
+                              Navigator.pop(context);
+                              _formKey.currentState?.reset();
+                            },
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                },
+                child: const Text('Register Foodie'),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
