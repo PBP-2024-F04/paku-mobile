f7a75d8a7640f359aea18e2a8690b2b68054cffd
secona <77039267+secona@users.noreply.github.com>
2024-12-07 23:57:52 +0700

    feat: implement comments with dummy comments

--- lib/screens/timeline/view_post.dart
+++ lib/screens/timeline/view_post.dart
@@ -1,9 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:paku/screens/timeline/models/comment.dart';
+import 'package:paku/screens/timeline/widgets/comment_card.dart';
 
 class ViewPostPage extends StatefulWidget {
-  const ViewPostPage(this.postId, {super.key});
+  ViewPostPage(this.postId, {super.key});
 
   final String postId;
+
+  final dummyComments = [
+    Comment.fromJson({
+      "id": "b13fbfa1-06f7-47d8-aca5-c1a26c1fb813",
+      "text": "test",
+      "username": "testmerchant",
+      "displayname": "Test",
+      "user_role": "Merchant",
+      "user_id": "3a2ee016-762a-4a1d-9074-c64b2bf9b77b",
+      "is_edited": false
+    }),
+    Comment.fromJson({
+      "id": "0eba2e2b-078e-4312-9ad7-57d850391712",
+      "text": "test",
+      "username": "testfoodie",
+      "displayname": "h",
+      "user_role": "Foodie",
+      "user_id": "99f1ae53-f503-4588-a42d-f0e8d54c4484",
+      "is_edited": false
+    }),
+    Comment.fromJson({
+      "id": "f2d9edb4-abd1-4e7f-bb10-1428bdb1bc40",
+      "text": "apa si",
+      "username": "testmerchant",
+      "displayname": "Test",
+      "user_role": "Merchant",
+      "user_id": "3a2ee016-762a-4a1d-9074-c64b2bf9b77b",
+      "is_edited": false
+    }),
+  ];
 
   @override
   State<ViewPostPage> createState() => _ViewPostPageState();
@@ -15,6 +47,21 @@ class _ViewPostPageState extends State<ViewPostPage> {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.postId),
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(30),
+        child: Center(
+          child: Column(
+            children: [
+              ListView.builder(
+                scrollDirection: Axis.vertical,
+                shrinkWrap: true,
+                itemCount: widget.dummyComments.length,
+                itemBuilder: (context, index) => CommentCard(widget.dummyComments[index]),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
