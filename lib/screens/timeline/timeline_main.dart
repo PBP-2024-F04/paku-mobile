59b2716baf71c00377bce5b386defd672ca947a8
secona <77039267+secona@users.noreply.github.com>
2024-12-08 14:08:30 +0700

    feat: implement view post

--- lib/screens/timeline/timeline_main.dart
+++ lib/screens/timeline/timeline_main.dart
@@ -3,57 +3,8 @@ import 'package:paku/colors.dart';
 import 'package:paku/screens/timeline/models/post.dart';
 import 'package:paku/screens/timeline/widgets/post_card.dart';
 import 'package:paku/widgets/left_drawer.dart';
-
-final dummyPosts = [
-  Post.fromJson({
-    "id": "1c5e6d6e-a302-40c1-b70c-6d66d5ebe8c0",
-    "user": {
-      "role": "Merchant",
-      "display_name": "Test",
-      "username": "testmerchant"
-    },
-    "text": "asdf",
-    "is_edited": false,
-    "created_at": "2024-10-27T08:17:53.424Z",
-    "updated_at": "2024-10-27T08:17:53.424Z"
-  }),
-  Post.fromJson({
-    "id": "fcc6bb7e-25c0-44e3-98b0-41982fe45ab1",
-    "user": {
-      "role": "Foodie",
-      "display_name": "123456789012345678901234567890",
-      "username": "testfoodie123"
-    },
-    "text": "Hello, World!",
-    "is_edited": false,
-    "created_at": "2024-10-27T04:12:26.175Z",
-    "updated_at": "2024-10-27T04:12:26.175Z"
-  }),
-  Post.fromJson({
-    "id": "932c5407-f2b5-49f4-b36f-844f75481e26",
-    "user": {"role": "Foodie", "display_name": "h", "username": "testfoodie"},
-    "text": "testsfdsfsdf",
-    "is_edited": true,
-    "created_at": "2024-10-27T03:40:22.018Z",
-    "updated_at": "2024-10-27T03:44:40.796Z"
-  }),
-  Post.fromJson({
-    "id": "788dfdf7-d8cb-45a1-ad1a-a78d6e4404bc",
-    "user": {"role": "Foodie", "display_name": "h", "username": "testfoodie"},
-    "text": "hello2",
-    "is_edited": false,
-    "created_at": "2024-10-27T03:39:24.107Z",
-    "updated_at": "2024-10-27T03:39:24.107Z"
-  }),
-  Post.fromJson({
-    "id": "5b5e9e8e-c22e-49ff-b33c-7d6554f2acf5",
-    "user": {"role": "Foodie", "display_name": "h", "username": "testfoodie"},
-    "text": "hello1",
-    "is_edited": false,
-    "created_at": "2024-10-27T03:39:13.465Z",
-    "updated_at": "2024-10-27T03:39:13.465Z"
-  }),
-];
+import 'package:pbp_django_auth/pbp_django_auth.dart';
+import 'package:provider/provider.dart';
 
 class TimelineMainPage extends StatefulWidget {
   const TimelineMainPage({super.key});
@@ -63,8 +14,20 @@ class TimelineMainPage extends StatefulWidget {
 }
 
 class _TimelineMainPageState extends State<TimelineMainPage> {
+  Future<List<Post>> _fetchPosts(CookieRequest request) async {
+    final response = await request.get('http://localhost:8000/timeline/json/posts');
+    
+    if (response is List<dynamic>) {
+      return response.map((data) => Post.fromJson(data)).toList();
+    }
+    
+    return [];
+  }
+
   @override
   Widget build(BuildContext context) {
+    final request = context.watch<CookieRequest>();
+
     return Scaffold(
       appBar: AppBar(title: const Text("Timeline")),
       drawer: const LeftDrawer(),
@@ -92,12 +55,28 @@ class _TimelineMainPageState extends State<TimelineMainPage> {
                   )
                 ],
               ),
-              ListView.builder(
-                scrollDirection: Axis.vertical,
-                shrinkWrap: true,
-                itemCount: dummyPosts.length,
-                itemBuilder: (context, index) => PostCard(dummyPosts[index]),
-              ),
+              FutureBuilder(
+                future: _fetchPosts(request),
+                builder: (context, AsyncSnapshot snapshot) {
+                  if (snapshot.data == null) {
+                    return const Center(child: CircularProgressIndicator());
+                  }
+
+                  if (snapshot.hasData) {
+                    return ListView.builder(
+                      scrollDirection: Axis.vertical,
+                      shrinkWrap: true,
+                      itemCount: snapshot.data.length,
+                      itemBuilder: (context, index) => PostCard(snapshot.data[index]),
+                    );
+                  } else {
+                    return Text(
+                      'Belum ada post.',
+                      style: Theme.of(context).textTheme.labelLarge,
+                    );
+                  }
+                },
+              )
             ],
           ),
         ),
