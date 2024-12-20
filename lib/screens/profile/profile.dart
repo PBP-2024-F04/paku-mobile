f4ac04a10ea72c71ba01fb1b5055027df7713039
secona <77039267+secona@users.noreply.github.com>
2024-12-21 01:05:14 +0700

    feat: add profile pages for merchant

--- lib/screens/profile/profile.dart
+++ lib/screens/profile/profile.dart
@@ -2,34 +2,47 @@ import 'package:flutter/material.dart';
 import 'package:paku/colors.dart';
 import 'package:paku/screens/profile/widgets/profile_favorites.dart';
 import 'package:paku/screens/profile/widgets/profile_posts.dart';
+import 'package:paku/screens/profile/widgets/profile_products.dart';
 import 'package:paku/screens/profile/widgets/profile_reviews.dart';
 
 class ProfilePage extends StatelessWidget {
+  final status = "Foodie";
+
   const ProfilePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-      length: 3,
+      length: status == "Foodie" ? 3 : 2,
       child: Scaffold(
         appBar: AppBar(
           title: const Text("Profile"),
-          bottom: const TabBar(
+          bottom: TabBar(
             labelColor: TailwindColors.whiteLight,
             unselectedLabelColor: TailwindColors.whiteLight,
-            tabs: [
-              Tab(icon: Icon(Icons.description)),
-              Tab(icon: Icon(Icons.reviews)),
-              Tab(icon: Icon(Icons.star)),
-            ],
+            tabs: status == "Foodie"
+                ? (const [
+                    Tab(icon: Icon(Icons.description)),
+                    Tab(icon: Icon(Icons.reviews)),
+                    Tab(icon: Icon(Icons.star)),
+                  ])
+                : (const [
+                    Tab(icon: Icon(Icons.description)),
+                    Tab(icon: Icon(Icons.shopping_cart)),
+                  ]),
           ),
         ),
-        body: const TabBarView(
-          children: [
-            ProfilePostsPage(),
-            ProfileReviewsPage(),
-            ProfileFavoritesPage(),
-          ],
+        body: TabBarView(
+          children: status == "Foodie"
+              ? (const [
+                  ProfilePostsPage(),
+                  ProfileReviewsPage(),
+                  ProfileFavoritesPage(),
+                ])
+              : (const [
+                  ProfilePostsPage(),
+                  ProfileProductsPage(),
+                ]),
         ),
       ),
     );
