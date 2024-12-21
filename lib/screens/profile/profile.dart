80428fdcc8ae4871499c51f367d62ce111dbd1fa
secona <77039267+secona@users.noreply.github.com>
2024-12-21 08:42:29 +0700

    feat: change profile layout

--- lib/screens/profile/profile.dart
+++ lib/screens/profile/profile.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:paku/colors.dart';
 import 'package:paku/screens/profile/widgets/profile_favorites.dart';
 import 'package:paku/screens/profile/widgets/profile_posts.dart';
 import 'package:paku/screens/profile/widgets/profile_products.dart';
 import 'package:paku/screens/profile/widgets/profile_reviews.dart';
+import 'package:paku/widgets/left_drawer.dart';
 
 class ProfilePage extends StatelessWidget {
-  final status = "Foodie";
+  final status = "Merchant";
 
   const ProfilePage({super.key});
 
@@ -16,33 +16,43 @@ class ProfilePage extends StatelessWidget {
       length: status == "Foodie" ? 3 : 2,
       child: Scaffold(
         appBar: AppBar(
-          title: const Text("Profile"),
-          bottom: TabBar(
-            labelColor: TailwindColors.whiteLight,
-            unselectedLabelColor: TailwindColors.whiteLight,
-            tabs: status == "Foodie"
-                ? (const [
-                    Tab(icon: Icon(Icons.description)),
-                    Tab(icon: Icon(Icons.reviews)),
-                    Tab(icon: Icon(Icons.star)),
-                  ])
-                : (const [
-                    Tab(icon: Icon(Icons.description)),
-                    Tab(icon: Icon(Icons.shopping_cart)),
-                  ]),
-          ),
+          title: const Text("h's Profile"),
         ),
-        body: TabBarView(
-          children: status == "Foodie"
-              ? (const [
-                  ProfilePostsPage(),
-                  ProfileReviewsPage(),
-                  ProfileFavoritesPage(),
-                ])
-              : (const [
-                  ProfilePostsPage(),
-                  ProfileProductsPage(),
-                ]),
+        drawer: const LeftDrawer(),
+        body: Column(
+          children: [
+            const SizedBox(height: 10.0),
+            Text("h", style: Theme.of(context).textTheme.displayMedium),
+            Text("@testfoodie", style: Theme.of(context).textTheme.bodySmall),
+            const SizedBox(height: 10.0),
+            TabBar(
+              labelStyle: Theme.of(context).textTheme.bodySmall,
+              tabs: status == "Foodie"
+                  ? (const [
+                      Tab(icon: Icon(Icons.description_outlined), text: "Posts"),
+                      Tab(icon: Icon(Icons.reviews_outlined), text: "Reviews"),
+                      Tab(icon: Icon(Icons.star_outline), text: "Favorites"),
+                    ])
+                  : (const [
+                      Tab(icon: Icon(Icons.description_outlined), text: "Posts"),
+                      Tab(icon: Icon(Icons.shopping_cart_outlined), text: "Products"),
+                    ]),
+            ),
+            Expanded(
+              child: TabBarView(
+                children: status == "Foodie"
+                    ? (const [
+                        ProfilePostsPage(),
+                        ProfileReviewsPage(),
+                        ProfileFavoritesPage(),
+                      ])
+                    : (const [
+                        ProfilePostsPage(),
+                        ProfileProductsPage(),
+                      ]),
+              ),
+            ),
+          ],
         ),
       ),
     );
