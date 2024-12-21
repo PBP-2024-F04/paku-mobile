28923ecd6e716b7de534b564c63b492bab0a61af
naphiale <nafia.levana19@gmail.com>
2024-12-22 03:05:59 +0700

    sorry gue kira cuma profile user

--- lib/screens/profile/widgets/profile_reviews.dart
+++ lib/screens/profile/widgets/profile_reviews.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:paku/screens/profile/models/profile.dart';
 import 'package:paku/screens/reviews/models/review.dart';
-import 'package:paku/screens/reviews/widgets/my_review_card.dart';
+import 'package:paku/screens/reviews/widgets/review_card.dart';
 import 'package:pbp_django_auth/pbp_django_auth.dart';
 import 'package:provider/provider.dart';
 import 'package:paku/screens/products/products.dart';
@@ -16,10 +16,9 @@ class ProfileReviewsPage extends StatefulWidget {
 }
 
 class _ProfileReviewsPageState extends State<ProfileReviewsPage> {
-  Future<List<Review>> _fetchMyReviews(
-      BuildContext context, CookieRequest request) async {
+  Future<List<Review>> _fetchReviews(BuildContext context, CookieRequest request) async {
     final response = await request.get(
-      'http://localhost:8000/reviews/json-reviews-me/',
+      'http://localhost:8000/profile/json/${widget.profile.username}/reviews',
     );
 
     if (response is List<dynamic>) {
@@ -29,7 +28,7 @@ class _ProfileReviewsPageState extends State<ProfileReviewsPage> {
     return [];
   }
 
-  Widget _myReviewsBuilder(
+  Widget _reviewsBuilder(
     BuildContext context,
     AsyncSnapshot<List<Review>> snapshot,
   ) {
@@ -44,7 +43,7 @@ class _ProfileReviewsPageState extends State<ProfileReviewsPage> {
         itemCount: snapshot.data!.length,
         itemBuilder: (context, index) {
           final review = snapshot.data![index];
-          return MyReviewCard(
+          return ReviewCard(
             review: review,
             onTap: () {
               Navigator.push(
@@ -68,14 +67,9 @@ class _ProfileReviewsPageState extends State<ProfileReviewsPage> {
   Widget build(BuildContext context) {
     final request = context.watch<CookieRequest>();
 
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('My Reviews'),
-      ),
-      body: FutureBuilder(
-        future: _fetchMyReviews(context, request),
-        builder: _myReviewsBuilder,
-      ),
+    return FutureBuilder(
+        future: _fetchReviews(context, request),
+        builder: _reviewsBuilder,
     );
   }
 }
