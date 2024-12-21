d59f9a992759d82fd653946d7ee4f21b9bb3dc24
secona <77039267+secona@users.noreply.github.com>
2024-12-21 20:08:03 +0700

    feat: left sidebar future

--- lib/widgets/left_drawer.dart
+++ lib/widgets/left_drawer.dart
@@ -8,49 +8,25 @@ import 'package:paku/screens/products/products.dart';
 import 'package:paku/screens/timeline/timeline_main.dart';
 import 'package:pbp_django_auth/pbp_django_auth.dart';
 
-Future<String> fetchUserRole(CookieRequest request) async {
-  try {
-    final response = await request.get('http://localhost:8000/accounts/json/get_user_role/');
-
-    if (response != null && response['role'] != null) {
-      return response['role'];
-    } else {
-      throw Exception('Role not found in response');
-    }
-  } catch (e) {
-    print('Error fetching user role: $e');
-    return 'Error fetching role';
-  }
-}
-
 class LeftDrawer extends StatefulWidget {
   const LeftDrawer({super.key});
 
   @override
-  _LeftDrawerState createState() => _LeftDrawerState();
+  State<LeftDrawer> createState() => _LeftDrawerState();
 }
 
 class _LeftDrawerState extends State<LeftDrawer> {
-  String? userRole;
+  Future<String?> _fetchUserRole(CookieRequest request) async {
+    final response = await request.get(
+      'http://localhost:8000/profile/json/',
+    );
 
-  @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      loadUserRole();
-    });
-  }
-
-  Future<void> loadUserRole() async {
-    final request = context.read<CookieRequest>(); 
-    final role = await fetchUserRole(request);
-    setState(() {
-      userRole = role;
-    });
+    return response["role"];
   }
 
   void _logout(BuildContext context, CookieRequest request) async {
-    final response = await request.logout("http://localhost:8000/accounts/auth/logout/");
+    final response =
+        await request.logout("http://localhost:8000/accounts/auth/logout/");
     String message = response["message"];
 
     if (context.mounted) {
@@ -77,141 +53,150 @@ class _LeftDrawerState extends State<LeftDrawer> {
 
   @override
   Widget build(BuildContext context) {
-    final request = context.read<CookieRequest>(); 
+    final request = context.read<CookieRequest>();
 
     return Drawer(
-      child: userRole == null
-          ? const Center(
-              child: CircularProgressIndicator(),
-            )
-          : ListView(
-              children: [
-                DrawerHeader(
-                  decoration: BoxDecoration(
-                    color: Theme.of(context).colorScheme.primary,
+      child: FutureBuilder(
+        future: _fetchUserRole(request),
+        builder: (context, AsyncSnapshot snapshot) => ListView(
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.primary,
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "PaKu",
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context)
+                        .textTheme
+                        .displaySmall
+                        ?.copyWith(color: Colors.white),
                   ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        "PaKu",
-                        textAlign: TextAlign.center,
-                        style: Theme.of(context)
-                            .textTheme
-                            .displaySmall
-                            ?.copyWith(color: Colors.white),
-                      ),
-                      Text(
-                        "Jelajahi kuliner Palu!",
-                        textAlign: TextAlign.center,
-                        style: Theme.of(context)
-                            .textTheme
-                            .labelLarge
-                            ?.copyWith(color: Colors.white),
-                      ),
-                    ],
-                  ),
-                ),
-                if (userRole == 'Foodie') ...[
-                  ListTile(
-                    leading: const Icon(Icons.food_bank_outlined),
-                    title: const Text('Products'),
-                    onTap: () {
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(builder: (context) => Products()),
-                      );
-                    },
-                  ),
-                  ListTile(
-                    leading: const Icon(Icons.home_outlined),
-                    title: const Text('Timeline'),
-                    onTap: () {
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(builder: (context) => const TimelineMainPage()),
-                      );
-                    },
-                  ),
-                  ListTile(
-                    leading: const Icon(Icons.star_border),
-                    title: const Text('Favorites'),
-                    onTap: () {
-                      // Navigasi ke halaman Favorites
-                    },
-                  ),
-                  ListTile(
-                    leading: const Icon(Icons.discount_outlined),
-                    title: const Text('Promos'),
-                    onTap: () {
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(builder: (context) => const Promos()),
-                      );
-                    },
-                  ),
-                  ListTile(
-                    leading: const Icon(Icons.book_outlined),
-                    title: const Text('Reviews'),
-                    onTap: () {
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(builder: (context) => ReviewPage()),
-                      );
-                    },
-                  ),
-                ] else if (userRole == 'Merchant') ...[
-                  ListTile(
-                    leading: const Icon(Icons.food_bank_outlined),
-                    title: const Text('My Products'),
-                    onTap: () {
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(builder: (context) => Products()),
-                      );
-                    },
-                  ),
-                  ListTile(
-                    leading: const Icon(Icons.home_outlined),
-                    title: const Text('Timeline'),
-                    onTap: () {
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(builder: (context) => const TimelineMainPage()),
-                      );
-                    },
-                  ),
-                  ListTile(
-                    leading: const Icon(Icons.discount_outlined),
-                    title: const Text('My Promos'),
-                    onTap: () {
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(builder: (context) => const MyPromos()),
-                      );
-                    },
-                  ),
-                  ListTile(
-                    leading: const Icon(Icons.book_outlined),
-                    title: const Text('Reviews'),
-                    onTap: () {
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(builder: (context) => ReviewPage()),
-                      );
-                    },
+                  Text(
+                    "Jelajahi kuliner Palu!",
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context)
+                        .textTheme
+                        .labelLarge
+                        ?.copyWith(color: Colors.white),
                   ),
                 ],
-                const Divider(),
-                ListTile(
-                  leading: const Icon(Icons.logout),
-                  title: const Text('Logout'),
-                  onTap: () {
-                    _logout(context, request);
-                  },
-                ),
-              ],
+              ),
             ),
+            if (snapshot.hasData)
+              if (snapshot.data == "Merchant")
+                ..._merchantButtons(context)
+              else
+                ..._foodieButtons(context)
+            else
+              const Center(child: CircularProgressIndicator()),
+            const Divider(),
+            ListTile(
+              leading: const Icon(Icons.logout),
+              title: const Text('Logout'),
+              onTap: () {
+                _logout(context, request);
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
+
+  List<Widget> _merchantButtons(BuildContext context) => [
+        ListTile(
+          leading: const Icon(Icons.food_bank_outlined),
+          title: const Text('My Products'),
+          onTap: () {
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (context) => Products()),
+            );
+          },
+        ),
+        ListTile(
+          leading: const Icon(Icons.home_outlined),
+          title: const Text('Timeline'),
+          onTap: () {
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (context) => const TimelineMainPage()),
+            );
+          },
+        ),
+        ListTile(
+          leading: const Icon(Icons.discount_outlined),
+          title: const Text('My Promos'),
+          onTap: () {
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (context) => const MyPromos()),
+            );
+          },
+        ),
+        ListTile(
+          leading: const Icon(Icons.book_outlined),
+          title: const Text('Reviews'),
+          onTap: () {
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (context) => ReviewPage()),
+            );
+          },
+        ),
+      ];
+
+  List<Widget> _foodieButtons(BuildContext context) => [
+        ListTile(
+          leading: const Icon(Icons.food_bank_outlined),
+          title: const Text('Products'),
+          onTap: () {
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (context) => Products()),
+            );
+          },
+        ),
+        ListTile(
+          leading: const Icon(Icons.home_outlined),
+          title: const Text('Timeline'),
+          onTap: () {
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (context) => const TimelineMainPage()),
+            );
+          },
+        ),
+        ListTile(
+          leading: const Icon(Icons.star_border),
+          title: const Text('Favorites'),
+          onTap: () {
+            // Navigasi ke halaman Favorites
+          },
+        ),
+        ListTile(
+          leading: const Icon(Icons.discount_outlined),
+          title: const Text('Promos'),
+          onTap: () {
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (context) => const Promos()),
+            );
+          },
+        ),
+        ListTile(
+          leading: const Icon(Icons.book_outlined),
+          title: const Text('Reviews'),
+          onTap: () {
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (context) => ReviewPage()),
+            );
+          },
+        ),
+      ];
 }
